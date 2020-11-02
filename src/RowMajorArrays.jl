97726fef5a56c417ef9deb88c9d6f3d87f876450
Benjamin Lungwitz <benj.lung@googlemail.com>
2020-11-02 10:37:50 +0100

    initial commit

--- src/RowMajorArrays.jl
+++ src/RowMajorArrays.jl
@@ -1,5 +1,31 @@
 module RowMajorArrays
 
-# Write your package code here.
+export RowMajorArray
+
+struct RowMajorArray{A <: AbstractArray}
+    data:: A
+end
+
+import Base
+
+Base.getindex(a:: RowMajorArray, I::Vararg{<: Integer, N}) where {N} = getindex(a.data, reverse(I)...)
+Base.getindex(a:: RowMajorArray, i:: Integer) = getindex(a.data, i) # linear indexing is done row-major here to have optimal memory alignment
+
+Base.setindex!(a:: RowMajorArray, v, i:: Integer) = setindex!(a.data, v, i)
+Base.setindex!(a:: RowMajorArray, v, I::Vararg{<: Integer, N}) where {N} = setindex!(a.data, v, reverse(I)...)
+
+Base.size(a:: RowMajorArray) = reverse(size(a.data))
+
+forward_methods = (:length, :eltype)
+for m in forward_methods
+    @eval Base.$m(a:: RowMajorArray) = Base.$m(a.data)
+end
+
+# separate from `Base.zeros`, etc. - execute with qualified module name, e.g. `RowMajorArrays.zeros`, etc.
+forward_methods = (:zeros, :ones, :rand, :randn)
+for m in forward_methods
+    @eval $m(T:: Type, I::Vararg{<: Integer, N}) where {N} = RowMajorArray(Base.$m(T, reverse(I)...))
+    @eval $m(I::Vararg{<: Integer, N}) where {N} = RowMajorArray(Base.$m(reverse(I)...))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,50 @@ using RowMajorArrays
 using Test
 
 @testset "RowMajorArrays.jl" begin
-    # Write your tests here.
+    a = [1 2 3; 4 5 6]
+    a_r = RowMajorArray(a)
+
+    @test a[1, 3] == a_r[3, 1]
+    @test size(a) == reverse(size(a_r)) == (2, 3)
+
+    setindex!(a_r, 42, 3, 2)
+    @test getindex(a_r, 3, 2) == 42
+    setindex!(a_r, 43, 2,1)
+    @test getindex(a_r, 2, 1) == 43
+
+    z = RowMajorArrays.zeros(Int, 5, 3)
+    @test size(z) == (5, 3)
+    @test zeros(Int, 3, 5) == z.data
+    @test eltype(z) == Int
+    # @test z .== 0
+
+    z2 = RowMajorArrays.zeros(5, 3)
+    @test size(z2) == (5, 3)
+    @test zeros(3, 5) == z.data
+    @test eltype(z2) == Float64
+    # @test z .== 0
+
+    o = RowMajorArrays.ones(Int, 5, 3)
+    @test size(o) == (5, 3)
+    @test ones(Int, 3, 5) == o.data
+    # @test o .== 0
+
+    r1 = RowMajorArrays.rand(7, 2)
+    @test size(r1.data) == (2, 7)
+    @test eltype(r1) == Float64
+    r1b = RowMajorArrays.rand(Float32, 7, 2)
+    @test size(r1b.data) == (2, 7)
+    @test eltype(r1b) == Float32
+
+    r2 = RowMajorArrays.randn(7, 2)
+    @test size(r2.data) == (2, 7)
+    @test eltype(r2) == Float64
+    r2b = RowMajorArrays.randn(Float32, 7, 2)
+    @test size(r2b.data) == (2, 7)
+    @test eltype(r2b) == Float32
+
+
+    
+
+
 end
